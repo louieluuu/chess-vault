070d95d4b59d5382e8d558ec8e10a2d249de4792
louieluuu <yi-jie.lu@hotmail.com>
2025-05-29 19:36:25 -0700

    [HELLO] Guess who's back, back again

--- src/main/index.js
+++ src/main/index.js
@@ -1,7 +1,7 @@
 import { app, shell, BrowserWindow, ipcMain } from 'electron'
 import { join } from 'path'
 import { electronApp, optimizer, is } from '@electron-toolkit/utils'
-import icon from '../../resources/icon.png?asset'
+import icon from '../renderer/src/assets/img/icon.png?asset'
 
 import Database from 'better-sqlite3'
 import fs from 'fs'
