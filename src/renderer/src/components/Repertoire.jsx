b343719a8d7f59d72ada6ca597850931e1292271
Thomas Chiu <tomchiu19@gmail.com>
2025-06-25 15:35:54 -0700

    feat: organize repertoire by opening

--- src/renderer/src/components/Repertoire.jsx
+++ src/renderer/src/components/Repertoire.jsx
@@ -1,16 +1,36 @@
 import Thumbnail from './Thumbnail'
 
 function Repertoire({ chess, repertoire, setFen, setOrientation }) {
+  const groupedRepertoire = repertoire.reduce((acc, item) => {
+    const openingName = item.opening
+    if (!acc[openingName]) {
+      acc[openingName] = []
+    }
+    acc[openingName].push(item)
+    return acc
+  }, {})
+
+  const sortedOpeningNames = Object.keys(groupedRepertoire).sort((nameA, nameB) =>
+    nameA.localeCompare(nameB)
+  )
+
   return (
     <div className="repertoire">
-      {repertoire.map((v) => (
-        <Thumbnail
-          key={v.id}
-          chess={chess}
-          variation={v}
-          setFen={setFen}
-          setOrientation={setOrientation}
-        />
+      {sortedOpeningNames.map((openingName) => (
+        <div key={openingName} className="opening-group">
+          <h3 className="opening-group__name">{openingName}</h3>
+          <div className="thumbnails-container">
+            {groupedRepertoire[openingName].map((v) => (
+              <Thumbnail
+                key={v.id}
+                chess={chess}
+                variation={v}
+                setFen={setFen}
+                setOrientation={setOrientation}
+              />
+            ))}
+          </div>
+        </div>
       ))}
     </div>
   )
